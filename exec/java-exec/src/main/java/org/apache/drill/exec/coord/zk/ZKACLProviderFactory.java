ff2a71b33c45a93889a998df09ae6866e42a81fe
karthik <kmanivannan@maprtech.com>
2018-07-19 10:34:03 -0700

    removed spacing

--- exec/java-exec/src/main/java/org/apache/drill/exec/coord/zk/ZKACLProviderFactory.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/coord/zk/ZKACLProviderFactory.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.drill.exec.coord.zk;
 
 import org.apache.curator.framework.api.ACLProvider;
