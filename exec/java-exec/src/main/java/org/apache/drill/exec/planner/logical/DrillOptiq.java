7ccaa989ef431aadfa1e5bad585be3451c2ca929
Charles S. Givre <cgivre@apache.org>
2021-12-18 18:03:20 -0500

    DRILL-8078: Add WEEK to Date Extract (#2407)
    (cherry picked from commit ef8ea00b89e5d58781e8659e860cf4d63b321662)

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/logical/DrillOptiq.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/logical/DrillOptiq.java
@@ -565,6 +565,7 @@ public class DrillOptiq {
           switch (timeUnit) {
             case YEAR:
             case MONTH:
+            case WEEK:
             case DAY:
             case HOUR:
             case MINUTE:
@@ -573,7 +574,7 @@ public class DrillOptiq {
               functionName += functionPostfix;
               return FunctionCallFactory.createExpression(functionName, args.subList(1, 2));
             default:
-              throw new UnsupportedOperationException("extract function supports the following time units: YEAR, MONTH, DAY, HOUR, MINUTE, SECOND");
+              throw new UnsupportedOperationException("extract function supports the following time units: YEAR, MONTH, WEEK, DAY, HOUR, MINUTE, SECOND");
           }
         }
         case "timestampdiff": {
