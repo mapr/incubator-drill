70c7c2bd817880f9d4c7c3d810e86ce0fff0a0e3
Charles S. Givre <cgivre@apache.org>
2022-04-07 14:10:37 -0400

    DRILL-8191: HTTP Request Function Not Detecting JSON Config (#2517)
    (cherry picked from commit 355e074858373ab7b42dd303bee9feebe4a671c0)

--- contrib/storage-http/src/main/java/org/apache/drill/exec/store/http/util/SimpleHttp.java
+++ contrib/storage-http/src/main/java/org/apache/drill/exec/store/http/util/SimpleHttp.java
@@ -833,7 +833,7 @@ public class SimpleHttp implements AutoCloseable {
       throw UserException.functionError()
         .message("You must call this function with a valid endpoint name.")
         .build(logger);
-    } else if (endpointConfig.inputType() != "json") {
+    } else if (! endpointConfig.inputType().contentEquals("json")) {
       throw UserException.functionError()
         .message("Http_get only supports API endpoints which return json.")
         .build(logger);
