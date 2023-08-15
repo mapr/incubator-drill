vataga/drill

DRILL-6953: Merge row set-based JSON reader

exec/java-exec/src/main/java/org/apache/drill/exec/store/dfs/easy/EasyFormatPlugin.java
@@ -50,7 +50,7 @@ import org.apache.drill.exec.planner.physical.PlannerSettings;
 import org.apache.drill.exec.record.CloseableRecordBatch;
 import org.apache.drill.exec.record.RecordBatch;
 import org.apache.drill.exec.server.DrillbitContext;
-import org.apache.drill.exec.server.options.OptionManager;
+import org.apache.drill.exec.server.options.OptionSet;
 import org.apache.drill.exec.store.ColumnExplorer;
 import org.apache.drill.exec.store.RecordReader;
 import org.apache.drill.exec.store.RecordWriter;
@@ -118,7 +118,7 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
     /**
      *  Choose whether to use the "traditional" or "enhanced" reader
      *  structure. Can also be selected at runtime by overriding
-     *  {@link #useEnhancedScan()}.
+     *  {@link #useEnhancedScan(OptionSet)}.
      */
     private final boolean useEnhancedScan;
 
@@ -502,7 +502,7 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
 
   protected CloseableRecordBatch getReaderBatch(FragmentContext context,
       EasySubScan scan) throws ExecutionSetupException {
-    if (useEnhancedScan()) {
+    if (useEnhancedScan(context.getOptions())) {
       return buildScan(context, scan);
     } else {
       return buildScanBatch(context, scan);
@@ -518,8 +518,8 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
    * @return true to use the enhanced scan framework, false for the
    * traditional scan-batch framework
    */
-  protected boolean useEnhancedScan() {
-    return easyConfig.useEnhancedScan();
+  protected boolean useEnhancedScan(OptionSet options) {
+    return easyConfig.useEnhancedScan;
   }
 
   /**
@@ -605,13 +605,13 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
   /**
    * Initialize the scan framework builder with standard options.
    * Call this from the plugin-specific
-   * {@link #frameworkBuilder(OptionManager, EasySubScan)} method.
+   * {@link #frameworkBuilder(OptionSet, EasySubScan)} method.
    * The plugin can then customize/revise options as needed.
    *
    * @param builder the scan framework builder you create in the
-   * {@link #frameworkBuilder(OptionManager, EasySubScan)} method
+   * {@link #frameworkBuilder(OptionSet, EasySubScan)} method
    * @param scan the physical scan operator definition passed to
-   * the {@link #frameworkBuilder(OptionManager, EasySubScan)} method
+   * the {@link #frameworkBuilder(OptionSet, EasySubScan)} method
    */
   protected void initScanBuilder(FileScanBuilder builder, EasySubScan scan) {
     builder.projection(scan.getColumns());
@@ -638,7 +638,7 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
   }
 
   public ManagedReader<? extends FileSchemaNegotiator> newBatchReader(
-      EasySubScan scan, OptionManager options) throws ExecutionSetupException {
+      EasySubScan scan, OptionSet options) throws ExecutionSetupException {
     throw new ExecutionSetupException("Must implement newBatchReader() if using the enhanced framework.");
   }
 
@@ -656,7 +656,7 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
    * @throws ExecutionSetupException for all setup failures
    */
   protected FileScanBuilder frameworkBuilder(
-      OptionManager options, EasySubScan scan) throws ExecutionSetupException {
+      OptionSet options, EasySubScan scan) throws ExecutionSetupException {
     throw new ExecutionSetupException("Must implement frameworkBuilder() if using the enhanced framework.");
   }
 

mapr/incubator-drill

* Snapshot: reworked JSON field parser creation
* Updated JSON loader
* Redo value listener with tokens
* Extended long type works
* More simple extended types
* Added $date
* Binary type
* All extended type except arrays
* Extended arrays partly working
* More arrays work
* Refactor element parser interfaces
* Rename RowSetTests --> RowSetTest
* More factory cleanup
* Revised unknown field creation
* In middle of factory/parser restructuring
* Scalars, object, some variants work again
* JSON loader tests pass
* File cleanup
* Old extended types test passes
* Renamed JSON packages
* Tested extended provided types
(cherry picked from commit b464b9991ac34f3a8ea293e62a819a2b69504ed1)

exec/java-exec/src/main/java/org/apache/drill/exec/store/dfs/easy/EasyFormatPlugin.java
@@ -48,7 +48,8 @@ import org.apache.drill.exec.planner.physical.PlannerSettings;
 import org.apache.drill.exec.record.CloseableRecordBatch;
 import org.apache.drill.exec.record.RecordBatch;
 import org.apache.drill.exec.server.DrillbitContext;
-import org.apache.drill.exec.server.options.OptionManager;
+import org.apache.drill.exec.server.options.OptionSet;
+import org.apache.drill.exec.store.ColumnExplorer;
 import org.apache.drill.exec.store.RecordReader;
 import org.apache.drill.exec.store.RecordWriter;
 import org.apache.drill.exec.store.StatisticsRecordWriter;
@@ -130,7 +131,7 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
     /**
      *  Choose whether to use the "traditional" or "enhanced" reader
      *  structure. Can also be selected at runtime by overriding
-     *  {@link #useEnhancedScan()}.
+     *  {@link #useEnhancedScan(OptionSet)}.
      */
     private final ScanFrameworkVersion scanVersion;
 
@@ -515,7 +516,7 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
    * @return true to use the enhanced scan framework, false for the
    * traditional scan-batch framework
    */
-  protected ScanFrameworkVersion scanVersion(OptionManager options) {
+  protected ScanFrameworkVersion scanVersion(OptionSet options) {
     return easyConfig.scanVersion;
   }
 
@@ -545,15 +546,15 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
   /**
    * Initialize the scan framework builder with standard options.
    * Call this from the plugin-specific
-   * {@link #frameworkBuilder(OptionManager, EasySubScan)} method.
+   * {@link #frameworkBuilder(OptionSet, EasySubScan)} method.
    * The plugin can then customize/revise options as needed.
    * <p>
    * For EVF V1, to be removed.
    *
    * @param builder the scan framework builder you create in the
-   * {@link #frameworkBuilder(OptionManager, EasySubScan)} method
+   * {@link #frameworkBuilder(OptionSet, EasySubScan)} method
    * @param scan the physical scan operator definition passed to
-   * the {@link #frameworkBuilder(OptionManager, EasySubScan)} method
+   * the {@link #frameworkBuilder(OptionSet, EasySubScan)} method
    */
   protected void initScanBuilder(FileScanBuilder builder, EasySubScan scan) {
     EvfV1ScanBuilder.initScanBuilder(this, builder, scan);
@@ -563,7 +564,7 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
    * For EVF V1, to be removed.
    */
   public ManagedReader<? extends FileSchemaNegotiator> newBatchReader(
-      EasySubScan scan, OptionManager options) throws ExecutionSetupException {
+      EasySubScan scan, OptionSet options) throws ExecutionSetupException {
     throw new ExecutionSetupException("Must implement newBatchReader() if using the enhanced framework.");
   }
 
@@ -583,7 +584,7 @@ public abstract class EasyFormatPlugin<T extends FormatPluginConfig> implements 
    * @throws ExecutionSetupException for all setup failures
    */
   protected FileScanBuilder frameworkBuilder(
-      OptionManager options, EasySubScan scan) throws ExecutionSetupException {
+      OptionSet options, EasySubScan scan) throws ExecutionSetupException {
     throw new ExecutionSetupException("Must implement frameworkBuilder() if using the enhanced framework.");
   }
 