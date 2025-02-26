a06f32adc5e3b784da641326f48a7c69095c041e
rymarm <rymarm@apache.org>
2025-02-26 12:44:13 +0200

    MD-6585: Add fast fail on missing OIDC property

--- exec/java-exec/src/main/java/org/apache/drill/exec/server/rest/auth/OpenIdSecurityHandler.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/server/rest/auth/OpenIdSecurityHandler.java
@@ -1,5 +1,6 @@
 package org.apache.drill.exec.server.rest.auth;
 
+import com.google.common.base.Strings;
 import org.apache.drill.common.exceptions.DrillException;
 import org.apache.drill.exec.server.DrillbitContext;
 import org.apache.drill.exec.server.rest.WebServerConstants;
@@ -11,6 +12,8 @@ public class OpenIdSecurityHandler extends DrillHttpConstraintSecurityHandler {
   public void doSetup(DrillbitContext dbContext) throws DrillException {
     OpenIdConfigurationProvider openIdConfigurationProvider =
         OpenIdConfigurationProvider.getProvider(dbContext.getConfig());
+    validateOpenIdConfiguration(openIdConfigurationProvider);
+
     OpenIdConfiguration oidcConfig = openIdConfigurationProvider.getConfiguration();
     String errorPage = WebServerConstants.MAIN_LOGIN_RESOURCE_PATH;
     String authenticationUri = WebServerConstants.OPEN_ID_LOGIN_RESOURCE_PATH;
@@ -27,5 +30,23 @@ public class OpenIdSecurityHandler extends DrillHttpConstraintSecurityHandler {
   @Override
   public String getImplName() {
     return Constraint.__OPENID_AUTH;
+  }
+
+  public void validateOpenIdConfiguration(OpenIdConfigurationProvider provider) {
+    StringBuilder errorMessage = new StringBuilder()
+        .append(provider.getClass().getName())
+        .append(". ");
+    if (Strings.isNullOrEmpty(provider.getClientIssuer())) {
+      errorMessage.append("OIDC client issuer is not provided");
+    } else if (Strings.isNullOrEmpty(provider.getClientId())) {
+      errorMessage.append("OIDC client Id is not provided");
+    } else if (Strings.isNullOrEmpty(provider.getClientSecret())) {
+      errorMessage.append("OIDC client secret is not provided");
+    } else if (Strings.isNullOrEmpty(provider.getUserAttrName())) {
+      errorMessage.append("OIDC user attribute name is not provided");
+    } else {
+      return;
+    }
+    throw new IllegalArgumentException(errorMessage.toString());
   }
 }
